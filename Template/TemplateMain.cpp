492fe59c2d17ff04ac18e08415eb68889f8c6577
quickgrid <qdevapps@gmail.com>
2015-01-31 18:22:07 -0800

    Inclusion of Sample SFML template project
    included sample SFML template project. It loads a specified font and
    shows in a given co-ordinate.

---
+++ Template/TemplateMain.cpp
@@ -0,0 +1,51 @@
+#include<SFML/Graphics.hpp>
+#include<iostream>
+using namespace sf;
+
+int main(){
+    //create game window
+    RenderWindow window(VideoMode(600,400), "Angry Flappy Zombie");
+
+    //run the program as long as window is open
+    while(window.isOpen()){
+        //check the events triggered in each iteration
+        Event event;
+        while(window.pollEvent(event)){
+            //window close action triggered
+            switch(event.type){
+            case Event::Closed:
+                window.close();
+                break;
+            }
+        }
+        //clear or paint the window with color of choice
+        window.clear(Color::White);
+
+        //draw methods or other actions here
+
+
+        //load font from file
+        Font font;
+        if(!font.loadFromFile("resources/fonts/sheandy.ttf")){
+            std::cout << "Error" << std::endl;
+        }
+
+        //setting font from different loaded fonts
+        Text text;
+        text.setFont(font);
+
+        //set font text and style the font
+        text.setString("Angry Flappy Zombie");
+        text.setCharacterSize(24);
+        text.setColor(Color::Red);
+        text.setPosition(100,200);
+
+        //show the font
+        window.draw(text);
+
+        //end the current frame
+        window.display();
+    }
+
+    return 0;
+}
